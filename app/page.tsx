d26f71298e7a0b14544934aa671d1dfd90885641
amadero170 <amadero170@gmail.com>
2024-01-12 09:38:34 -0600

    cinco

--- app/page.tsx
+++ app/page.tsx
@@ -5,74 +5,74 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva1.jpeg"
+        src="/Diapositiva1.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva2.jpeg"
+        src="/Diapositiva2.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva3.jpeg"
+        src="/Diapositiva3.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva4.jpeg"
+        src="/Diapositiva4.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva5.jpeg"
+        src="/Diapositiva5.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva6.jpeg"
+        src="/Diapositiva6.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva7.jpeg"
+        src="/Diapositiva7.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva8.jpeg"
+        src="/Diapositiva8.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
       <Image
         className="w-full max-w-2xl"
-        src="Diapositiva9.jpeg"
+        src="/Diapositiva9.jpeg"
         alt="rifa"
-        width={180}
-        height={37}
+        width={1000}
+        height={1000}
         priority
       />
     </main>
